ff819f623f5bf1dcc2e59c1318a719d79f8a5b7f
Aachaman <144558579+Achamanp@users.noreply.github.com>
2025-01-17 03:07:34 +0530

    Initial commit

---
+++ chat-app-main/chat-app-backend/src/main/java/com/substring/chat/playload/MessageRequest.java
@@ -0,0 +1,49 @@
+package com.substring.chat.playload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageRequest {
+
+    private String content;
+    private String sender;
+    private String roomId;
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getSender() {
+		return sender;
+	}
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	public String getRoomId() {
+		return roomId;
+	}
+	public void setRoomId(String roomId) {
+		this.roomId = roomId;
+	}
+	public MessageRequest(String content, String sender, String roomId) {
+		super();
+		this.content = content;
+		this.sender = sender;
+		this.roomId = roomId;
+	}
+	public MessageRequest() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
